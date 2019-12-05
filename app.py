2bb5e384cd26d153bcb1795dfee6d8f37e1b714e
karnaukhova <katyakarnaukhova@yandex.ru>
2019-12-05 19:52:08 +0500

    heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from models import db, Men
+from models import db, Employee, Position
 from routes import api, index
 
 app = Flask(__name__)
@@ -9,11 +9,15 @@ app.register_blueprint(index)
 db.init_app(app)
 
 with app.app_context():
-  db.create_all(app=app)
-  db.session.add(Men(name='Bod'))
-  db.session.add(Men(name='Rocky'))
-  db.session.commit()
-
+    db.create_all()
+    manager = Position(name='Manager')
+    developer = Position(name='Developer')
+    db.session.add(manager)
+    db.session.add(developer)
+    db.session.commit()
+    db.session.add(Employee(name='Bob', position_id=manager.id))
+    db.session.add(Employee(name='Rocky', position_id=developer.id))
+    db.session.commit()
 
 if __name__ == '__main__':
   app.run()
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 
-from models import Men, db
+from models import Employee, db
 
 index = Blueprint('index', __name__,url_prefix='/')
 api = Blueprint('api', __name__,url_prefix='/api')
@@ -9,16 +9,16 @@ api = Blueprint('api', __name__,url_prefix='/api')
 
 @api.route('/mens')
 def get_mens():
-    return jsonify([(lambda men: men.json())(men) for men in Men.query.all()])
+    return jsonify([(lambda men: men.json())(men) for men in Employee.query.all()])
 
 @api.route('/men/id/<int:men_id>')
 def get_men(men_id):
-    men = Men.query.get(men_id)
+    men = Employee.query.get(men_id)
     return jsonify(men.json())if men else ''
 
 @api.route('/men/name/<string:men_name>/')
 def put_men(men_name):
-    men = Men(name=men_name)
+    men = Employee(name=men_name)
     db.session.add(men)
     db.session.commit()
     return jsonify(men.json())
