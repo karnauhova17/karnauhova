29a4968534fc53d8f7975710abfaf30b855c8303
karnaukhova <katyakarnaukhova@yandex.ru>
2019-12-10 20:41:39 +0500

    1

--- app.py
+++ app.py
@@ -23,16 +23,21 @@ with app.app_context():
     # Клиенты:
     clientKatya = Client(firstname='Ekaterina', lastname='Petrova', patronymic='Alexandrovna')
     db.session.add(clientKatya)
+    clientAnna2 = Client(firstname='Anna', lastname='Ivanova', patronymic='Andreevna')
+    db.session.add(clientAnna2)
+
 
     # Тренеры:
     mentorVasya = Mentor(firstname='Vasiliy', lastname='Pavlov', patronymic='Petrovich')
     db.session.add(mentorVasya)
+    mentorAndrey2 = Mentor(firstname='Andrey', lastname='Istomin', patronymic='Ivanov')
+    db.session.add( mentorAndrey2)
 
     # Сохраним типы тренировок, клиентов и тренеров:
     db.session.commit()
 
     # Посещения:
-    visit = Visit(client_id = clientKatya.id, mentor_id = mentorVasya.id, trainingtype_id=singleTraining.id, visit_date=datetime.datetime.now())
+    visit = Visit(client_id =  clientAnna2.id, mentor_id = mentorAndrey2.id, trainingtype_id=singleTraining.id, visit_date=datetime.datetime.now())
     db.session.add(visit)
     db.session.commit()
 
